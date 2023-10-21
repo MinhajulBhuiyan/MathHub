6d50527a47b752f792d54c573d0c64fd7b253b63
mdhralif <hasiburrahman999.alif@gmail.com>
2023-10-21 22:54:02 +0600

    correcting the trigonometric.h

--- trigonometry.h
+++ trigonometry.h
@@ -23,7 +23,8 @@ namespace trigonometry
         double factorial(int n)
         {
             double fact = 1.0;
-            for (int i = 2; i <= n; ++i) {
+            for (int i = 2; i <= n; ++i)
+            {
                 fact *= i;
             }
             return fact;
@@ -34,7 +35,8 @@ namespace trigonometry
         {
             double x = degreesToRadians(degrees); // Convert angle from degrees to radians
             double result = 0.0;
-            for (int i = 0; i < 50; ++i) {
+            for (int i = 0; i < 50; ++i)
+            {
                 result += pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
             }
             return result;
@@ -52,7 +54,8 @@ namespace trigonometry
             double sine = sinDegrees(degrees);
             double cosine = cosDegrees(degrees);
 
-            if (cosine < 1e-10 && cosine > -1e-10) {
+            if (cosine < 1e-10 && cosine > -1e-10)
+            {
                 cerr << "Error: tan(" << degrees << ") is undefined (division by zero)." << endl;
                 return 1.0 / 0.0; // Return positive infinity for undefined values
             }
@@ -60,37 +63,84 @@ namespace trigonometry
             return sine / cosine;
         }
 
-        // Calculate arcsine (inverse sine) using normal numbers (not degrees)
+        // Calculate arcsine (inverse sine) using approximation
         double asinNormal(double value)
         {
-            if (value < -1.0 || value > 1.0) {
+            if (value < -1.0 || value > 1.0)
+            {
                 cerr << "Error: asin(" << value << ") is undefined (out of range)." << endl;
                 return NAN; // Return NaN for undefined values
             }
+            else if (value == -1.0)
+            {
+                return -90.0; // arcsin(-1) is -90 degrees
+            }
+            else if (value == 1.0)
+            {
+                return 90.0; // arcsin(1) is 90 degrees
+            }
+            else
+            {
 
-            return asin(value) * 180.0 / PI;
+                double result = value;
+                double term = value;
+                int n = 1;
+
+                while (fabs(term) > 1e-15)
+                {
+                    term *= (value * value * (2 * n - 1) * (2 * n - 1)) / ((2 * n) * (2 * n + 1));
+                    result += term;
+                    n++;
+                }
+
+                return result * 180.0 / PI;
+            }
         }
 
-        // Calculate arccosine (inverse cosine) using normal numbers (not degrees)
+        // Calculate arccosine (inverse cosine) using approximation
         double acosNormal(double value)
         {
-            if (value < -1.0 || value > 1.0) {
-                cerr << "Error: acos(" << value << ") is undefined (out of range)." << endl;
+            if (value < -1.0 || value > 1.0)
+            {
+                cerr << "Error: arccos(" << value << ") is undefined (out of range)." << endl;
                 return NAN; // Return NaN for undefined values
             }
-
-            return acos(value) * 180.0 / PI;
+            else if (value == -1.0)
+            {
+                return 180.0; // arccos(-1) is 180 degrees
+            }
+            else if (value == 1.0)
+            {
+                return 0.0; // arccos(1) is 0 degrees
+            }
+            else
+            {
+                return 90.0 - asinNormal(value);
+            }
         }
 
-        // Calculate arctangent (inverse tangent) using normal numbers (not degrees)
+        // Calculate arctangent (inverse tangent) using approximation
         double atanNormal(double value)
         {
-            return atan(value) * 180.0 / PI;
+            if (value == 0.0)
+            {
+                return 0.0; // arctan(0) is 0 degrees
+            }
+            else if (value == -1.0)
+            {
+                return -45.0; // arctan(-1) is -45 degrees
+            }
+            else
+            {
+                double result = asinNormal(value / sqrt(1 + value * value));
+                return result;
+            }
         }
 
         void performTrigonometry()
         {
-            while (true) {
+            while (true)
+            {
                 int choice;
                 double angle;
 
@@ -105,62 +155,83 @@ namespace trigonometry
                 std::cout << "Enter your choice (0/1/2/3/4/5/6): ";
                 std::cin >> choice;
 
-                if (choice == 0) {
+                if (choice == 0)
+                {
                     break; // Return to the previous menu
-                } else if (choice >= 1 && choice <= 6) {
+                }
+                else if (choice >= 1 && choice <= 6)
+                {
                     std::cout << "Enter the angle or value: ";
                     std::cin >> angle;
 
-                    switch (choice) {
-                        case 1: {
-                            double result = sinDegrees(angle);
-                            if (std::abs(result) < 1e-15) {
-                                result = 0.0;
-                            }
-                            std::cout << "Sine(" << angle << ") = " << std::fixed << result << std::endl;
-                            break;
+                    switch (choice)
+                    {
+                    case 1:
+                    {
+                        double result = sinDegrees(angle);
+                        if (std::abs(result) < 1e-15)
+                        {
+                            result = 0.0;
                         }
-                        case 2: {
-                            double result = cosDegrees(angle);
-                            if (std::abs(result) < 1e-15) {
-                                result = 0.0;
-                            }
-                            std::cout << "Cosine(" << angle << ") = " << std::fixed << result << std::endl;
-                            break;
-                        }
-                        case 3: {
-                            double result = tanDegrees(angle);
-                            if (std::abs(result) < 1e-15) {
-                                result = 0.0;
-                            }
-                            std::cout << "Tangent(" << angle << ") = " << std::fixed << result << std::endl;
-                            break;
-                        }
-                        case 4: {
-                            double result = asinNormal(angle);
-                            if (std::isnan(result)) {
-                                std::cout << "arcsin(" << angle << ") is undefined (out of range)." << endl;
-                            } else {
-                                std::cout << "arcsin(" << angle << ") = " << std::fixed << result << " degrees" << std::endl;
-                            }
-                            break;
-                        }
-                        case 5: {
-                            double result = acosNormal(angle);
-                            if (std::isnan(result)) {
-                                std::cout << "arccos(" << angle << ") is undefined (out of range)." << endl;
-                            } else {
-                                std::cout << "arccos(" << angle << ") = " << std::fixed << result << " degrees" << std::endl;
-                            }
-                            break;
-                        }
-                        case 6: {
-                            double result = atanNormal(angle);
-                            std::cout << "arctan(" << angle << ") = " << std::fixed << result << " degrees" << std::endl;
-                            break;
-                        }
+                        std::cout << "Sine(" << angle << ") = " << std::fixed << result << std::endl;
+                        break;
                     }
-                } else {
+                    case 2:
+                    {
+                        double result = cosDegrees(angle);
+                        if (std::abs(result) < 1e-15)
+                        {
+                            result = 0.0;
+                        }
+                        std::cout << "Cosine(" << angle << ") = " << std::fixed << result << std::endl;
+                        break;
+                    }
+                    case 3:
+                    {
+                        double result = tanDegrees(angle);
+                        if (std::abs(result) < 1e-15)
+                        {
+                            result = 0.0;
+                        }
+                        std::cout << "Tangent(" << angle << ") = " << std::fixed << result << std::endl;
+                        break;
+                    }
+                    case 4:
+                    {
+                        double result = asinNormal(angle);
+                        if (std::isnan(result))
+                        {
+                            std::cout << "arcsin(" << angle << ") is undefined (out of range)." << endl;
+                        }
+                        else
+                        {
+                            std::cout << "arcsin(" << angle << ") = " << std::fixed << result << " degrees" << std::endl;
+                        }
+                        break;
+                    }
+                    case 5:
+                    {
+                        double result = acosNormal(angle);
+                        if (std::isnan(result))
+                        {
+                            std::cout << "arccos(" << angle << ") is undefined (out of range)." << endl;
+                        }
+                        else
+                        {
+                            std::cout << "arccos(" << angle << ") = " << std::fixed << result << " degrees" << std::endl;
+                        }
+                        break;
+                    }
+                    case 6:
+                    {
+                        double result = atanNormal(angle);
+                        std::cout << "arctan(" << angle << ") = " << std::fixed << result << " degrees" << std::endl;
+                        break;
+                    }
+                    }
+                }
+                else
+                {
                     std::cout << "\nInvalid choice for the trigonometric function. Please enter 0, 1, 2, 3, 4, 5, or 6.\n";
                 }
             }
