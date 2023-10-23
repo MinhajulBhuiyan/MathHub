1c011aa2f65676144856652a9bd12b079a902fbb
mdhralif <hasiburrahman999.alif@gmail.com>
2023-10-23 21:54:36 +0600

    Changes on Logarithmic functions

--- conversion.h
+++ conversion.h
@@ -1,134 +1,61 @@
 #ifndef CONVERSION_H_
 #define CONVERSION_H_
 #include <iostream>
-#include <map>
 
 using namespace std;
 namespace unit
 {
-    enum class LengthUnit
-    {
-        Inches,
-        Meters,
-        Feet,
-        Miles,
-        Centimeters,
-        Kilometers,
-        Yards,
-        NauticalMiles
-    };
-
-    map<string, LengthUnit> unitMap =
-        {
-            {"in", LengthUnit::Inches},
-            {"m", LengthUnit::Meters},
-            {"ft", LengthUnit::Feet},
-            {"mile", LengthUnit::Miles},
-            {"cm", LengthUnit::Centimeters},
-            {"km", LengthUnit::Kilometers},
-            {"nmile", LengthUnit::NauticalMiles},
-            {"yd", LengthUnit::Yards},
-    };
-
-    string toLower(const string &str)
-    {
-        string lowerStr = str;
-        for (char &c : lowerStr)
-        {
-            if (c >= 'A' && c <= 'Z')
-            {
-                c = static_cast<char>(c + 32);
-            }
-        }
-        return lowerStr;
-    }
-
     class LengthConversion
     {
-        
     public:
-        LengthConversion(double value, LengthUnit sourceUnit, LengthUnit targetUnit)
+        double inchesToCentimeters(double inches)
         {
-            this->value = value;
-            this->sourceUnit = sourceUnit;
-            this->targetUnit = targetUnit;
+            return inches * 2.54;
         }
 
-        double convert()
+        double centimetersToInches(double cm)
         {
-            double valueInMeters = convertToMeter();
-            double result = 0.0;
-
-            switch (targetUnit)
-            {
-            case LengthUnit::Inches:
-                result = valueInMeters / inchToMeter;
-                break;
-            case LengthUnit::Meters:
-                result = valueInMeters;
-                break;
-            case LengthUnit::Feet:
-                result = valueInMeters / footToMeter;
-                break;
-            case LengthUnit::Miles:
-                result = valueInMeters / mileToMeter;
-                break;
-            case LengthUnit::Centimeters:
-                result = valueInMeters / centimeterToMeter;
-                break;
-            case LengthUnit::Kilometers:
-                result = valueInMeters / kilometerToMeter;
-                break;
-            case LengthUnit::Yards:
-                result = valueInMeters / yardToMeter;
-                break;
-            case LengthUnit::NauticalMiles:
-                result = valueInMeters / nauticalMileToMeter;
-                break;
-            default:
-                cerr << "Unsupported target unit." << endl;
-            }
-
-            return result;
+            return cm / 2.54;
         }
 
-    private:
-        double value;
-        LengthUnit sourceUnit;
-        LengthUnit targetUnit;
-
-        const double inchToMeter = 0.0254;
-        const double footToMeter = 0.3048;
-        const double mileToMeter = 1609.34;
-        const double centimeterToMeter = 0.01;
-        const double kilometerToMeter = 1000.0;
-        const double yardToMeter = 0.9144;
-        const double nauticalMileToMeter = 1852.0;
-
-        double convertToMeter()
+        double feetToMeters(double feet)
         {
-            switch (sourceUnit)
-            {
-            case LengthUnit::Inches:
-                return value * inchToMeter;
-            case LengthUnit::Meters:
-                return value;
-            case LengthUnit::Feet:
-                return value * footToMeter;
-            case LengthUnit::Miles:
-                return value * mileToMeter;
-            case LengthUnit::Centimeters:
-                return value * centimeterToMeter;
-            case LengthUnit::Kilometers:
-                return value * kilometerToMeter;
-            case LengthUnit::Yards:
-                return value * yardToMeter;
-            case LengthUnit::NauticalMiles:
-                return value * nauticalMileToMeter;
-            default:
-                cerr << "Unsupported source unit." << endl;
-                return 0.0;
-            }
+            return feet * 0.3048;
+        }
+
+        double metersToFeet(double meters)
+        {
+            return meters / 0.3048;
+        }
+
+        double yardsToMeters(double yards)
+        {
+            return yards * 0.9144;
+        }
+
+        double metersToYards(double meters)
+        {
+            return meters / 0.9144;
+        }
+
+        double milesToKilometers(double miles)
+        {
+            return miles / 0.621371;
+        }
+
+        double kilometersToMiles(double kilometers)
+        {
+            return kilometers * 0.621371;
+        }
+
+        double nauticalMilesToMiles(double nauticalMiles)
+        {
+            return nauticalMiles * 1.15078;
+        }
+
+        double milesToNauticalMiles(double miles)
+        {
+            return miles / 1.15078;
         }
     };
 
@@ -291,44 +218,85 @@ namespace unit
     private:
         void performLengthConversions()
         {
+            unit::LengthConversion lengthConversion;
+            int subChoice;
             double value;
-            string sourceUnitStr, targetUnitStr;
 
-            cout << "Give input like this." << endl;
-            cout << "'m', 'cm', 'km', 'in', 'ft', 'yd', 'mile', 'nmile'" << endl;
-            cout << "Enter the source unit: ";
-            cin >> sourceUnitStr;
-
-            cout << "Enter the target unit: ";
-            cin >> targetUnitStr;
-
-            cout << "Enter the value: ";
-            cin >> value;
-
-            sourceUnitStr = toLower(sourceUnitStr);
-            targetUnitStr = toLower(targetUnitStr);
-
-            cout << "------------------------------------------------------------------------------------------------------------------------\n\n";
-
-            if (unitMap.find(sourceUnitStr) != unitMap.end() && unitMap.find(targetUnitStr) != unitMap.end())
+            while (true)
             {
-                LengthUnit sourceUnit = unitMap[sourceUnitStr];
-                LengthUnit targetUnit = unitMap[targetUnitStr];
+                cout << "\n\nLength Conversion Menu:\n\n";
+                cout << "1. in -> cm\n";
+                cout << "2. cm -> in\n";
+                cout << "3. ft -> m\n";
+                cout << "4. m -> ft\n";
+                cout << "5. yd -> m\n";
+                cout << "6. m -> yd\n";
+                cout << "7. mile -> km\n";
+                cout << "8. km -> mile\n";
+                cout << "9. n mile -> mile\n";
+                cout << "10. mile -> n mile\n";
+                cout << "0. Back to Main Menu\n\n";
+                cout << "Enter your choice (0-10): ";
 
-                LengthConversion converter(value, sourceUnit, targetUnit);
-                double result = converter.convert();
+                cin >> subChoice;
 
-                cout << value << " " << sourceUnitStr << " = " << result << " " << targetUnitStr << endl;
+                if (subChoice == 0)
+                {
+                    break; // Return to the main menu
+                }
+
+                cout << "\n\nEnter the value to convert: ";
+                cin >> value;
+
+                switch (subChoice)
+                {
+                case 1:
+                    cout << "\n\n\n"
+                         << value << " in = " << lengthConversion.inchesToCentimeters(value) << " cm.\n\n\n\n";
+                    break;
+                case 2:
+                    cout << "\n\n\n"
+                         << value << " cm = " << lengthConversion.centimetersToInches(value) << " in.\n\n\n\n";
+                    break;
+                case 3:
+                    cout << "\n\n\n"
+                         << value << " ft = " << lengthConversion.feetToMeters(value) << " m.\n\n\n\n";
+                    break;
+                case 4:
+                    cout << "\n\n\n"
+                         << value << " m = " << lengthConversion.metersToFeet(value) << " ft.\n\n\n\n";
+                    break;
+                case 5:
+                    cout << "\n\n\n"
+                         << value << " yd = " << lengthConversion.yardsToMeters(value) << " m.\n\n\n\n";
+                    break;
+                case 6:
+                    cout << "\n\n\n"
+                         << value << " m = " << lengthConversion.metersToYards(value) << " yd.\n\n\n\n";
+                    break;
+                case 7:
+                    cout << "\n\n\n"
+                         << value << " mile = " << lengthConversion.milesToKilometers(value) << " km.\n\n\n\n";
+                    break;
+                case 8:
+                    cout << "\n\n\n"
+                         << value << " km = " << lengthConversion.kilometersToMiles(value) << " mile.\n\n\n\n";
+                    break;
+                case 9:
+                    cout << "\n\n\n"
+                         << value << " n mile = " << lengthConversion.nauticalMilesToMiles(value) << " mile.\n\n\n\n";
+                    break;
+                case 10:
+                    cout << "\n\n\n"
+                         << value << " mile = " << lengthConversion.milesToNauticalMiles(value) << " n mile.\n\n\n\n";
+                    break;
+                default:
+                    cout << "\n\n\n"
+                         << "Invalid choice. Please enter a valid option.\n\n";
+                    break;
+                }
             }
-            else
-            {
-                cerr << "Unsupported source or target unit." << endl;
-            }
-
-            cout << "\n\n------------------------------------------------------------------------------------------------------------------------\n\n";
         }
-
-        
 
         void performTemperatureConversions()
         {
